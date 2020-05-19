2a59afd5cc1456b3ef392a97f556f4d039921282
Tadayuki Onishi <tt.tanishi100@gmail.com>
2020-05-20 00:50:41 +0900

    fix dev settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,22 +4,21 @@ module.exports = {
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:prettier/recommended",
-    "prettier/@typescript-eslint"
+    "prettier/@typescript-eslint",
   ],
   plugins: ["@typescript-eslint", "prettier", "jest"],
   env: {
     node: true,
     es6: true,
-    browser: true,
-    "jest/globals": true
+    "jest/globals": true,
   },
   parser: "@typescript-eslint/parser",
   parserOptions: {
     tsconfigRootDir: ".",
-    project: "./tsconfig.json"
+    project: "./tsconfig.json",
   },
   rules: {
     "prettier/prettier": "error",
-    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }]
-  }
+    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+  },
 };
